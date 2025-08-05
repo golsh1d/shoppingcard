43ab6e283bf07a0edb5ece96661a6c36d1026765
golshid <golsheedd@gmail.com>
2025-08-05 23:56:38 +0330

    remove card component

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,10 @@
 import Home from "./pages/Home/Home";
 import Signing from "./pages/Signing/Signing";
-import Cart from "./pages/Cart/Cart";
 import SingleProduct from "./pages/SingleProduct/SingleProduct"
 
 let routes = [
   { path: "/", element: <Home /> },
   { path: "/signing", element: <Signing /> },
-  { path: "/cart", element: <Cart /> },
   { path: "/singleProduct/:productID", element: <SingleProduct /> },
 ];
 
